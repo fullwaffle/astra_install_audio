180d6ea73bbd8a423e45559fefe61bfe1b9c7d52
fullwaffle <84801346+fullwaffle@users.noreply.github.com>
2023-10-28 21:00:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import subprocess
 
 
-def get_product_name():
+def get_product_name() -> str:
+    """
+    Функция для получения имени baseboard
+
+    :return: Возвращает имя baseboard
+    """
     command = f"dmidecode -t baseboard | grep -i 'Product Name' | sed 's/\tProduct Name: //' | tr -d '\n'"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
-    return result
+    return result.stdout
 
 
-def install_package(package_name):
-    subprocess.run(["apt-get", "update", "-y"])
+def install_package(package_name: str) -> None:
+    """
+    Функция обновляет репозитории и устанавливает пакет
+
+    :param package_name: Пакет для установки звука на NS685Is4
+    :return: None
+    """
+    subprocess.run(["apt-get", "update"])
     command = ["apt-get", "install", "-y", package_name]
     subprocess.run(command)
 
 
-def delete_package(package_name):
+def delete_package(package_name: str) -> None:
+    """
+    Функция удаляет пакет и его зависимости
+
+    :param package_name: Пакет для установки звука на NS685Is4
+    :return: None
+    """
     subprocess.run(["apt-get", "remove", "-y", package_name])
     subprocess.run(["apt-get", "autoremove", "-y"])
 
 
 if __name__ == "__main__":
-    product_name = get_product_name()
-    if product_name.stdout == "NS685Is4":
-        install_package("bsp-ns685-astra")
+    product_name: str = get_product_name()
+    if product_name == "NS685Is4":
+        install_package("bsp-ns685-astra")  # Установка звука на Astra Linux 1.7.4 для NS685Is4 ноута
     else:
         delete_package("bsp-ns685-astra")
